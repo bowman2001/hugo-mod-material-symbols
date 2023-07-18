044621438d593922fac61615c432a76508a9c6ff
Georg Makowski <gm@desider.at>
2023-07-18 12:07:38 +0200

    feat: new small symbols fix: viewBox

--- get_svgs.sh
+++ get_svgs.sh
@@ -3,8 +3,8 @@ SVGDIR="/home/georg/www/material-design-icons/symbols/web"
 VERSION="outlined"
 WEIGHT=300
 GRAD="0"  # "N25", "0", "200"
-SIZE=40 # 20, 24, 40, 48
-FILL=0  # 0, 1
+SIZE=20 # 20, 24, 40, 48
+FILL=1  # 0, 1
 
 TARGET="/home/georg/www/hugo-mod-material-symbols/assets/svg/material-symbols/${VERSION}/wght${WEIGHT}grad${GRAD}fill${FILL}opsz${SIZE}"
 mkdir -p ${TARGET} || exit
