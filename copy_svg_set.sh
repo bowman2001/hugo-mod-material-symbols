b479800a9940a4e6b446818cee3ff8c669f7ca4e
Georg Makowski <gm@desider.at>
2023-12-06 16:17:37 +0100

    chore: update icons

--- copy_svg_set.sh
+++ copy_svg_set.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 SVGDIR="/home/georg/www/material-design-icons/symbols/web"
-VERSION="outlined"
-WEIGHT=500 # 100 - 700
+VERSION="sharp"
+WEIGHT=300 # 100 - 700
 GRAD="0"  # "N25", "0", "200"
-SIZE=20 # 20, 24, 40, 48
-FILL=1  # 0, 1
+SIZE=48 # 20, 24, 40, 48
+FILL=0  # 0, 1
 
 TARGET="/home/georg/www/hugo-mod-material-symbols/assets/svg/material-symbols/${VERSION}/wght${WEIGHT}grad${GRAD}fill${FILL}opsz${SIZE}"
 
@@ -46,6 +46,6 @@ cd "${SVGDIR}" || exit
 for dir in *
 do
   cd "${dir}/materialsymbols${VERSION}" || exit
-  find . -name "${dir}${set}*" -exec cp {} "${TARGET}/${dir}.svg" \;
+  find . -name "${dir}${set}*" -exec sed -e 's/height="20" width="20"/viewBox="0 0 20 20"/' -e 's/height="24" width="24"/viewBox="0 0 24 24"/' -e 's/height="40" width="40"/viewBox="0 0 40 40"/' -e 's/height="48" width="48"/viewBox="0 0 48 48"/' -e 's/height=".." //' -e 's/ width=".."//' {} > "${TARGET}/${dir}.svg" \;
   cd "${SVGDIR}" || exit
 done
